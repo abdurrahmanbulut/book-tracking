586336f39c5a446356aa1f461d774101539fe0a5
Abdurrahman BULUT <abdurrahmanbulut08@gmail.com>
2024-07-14 20:51:57 +0300

    sherlock update

--- app/src/main/java/com/abdurrahmanbulut/composeAppBase/ui/MainActivity.kt
+++ app/src/main/java/com/abdurrahmanbulut/composeAppBase/ui/MainActivity.kt
@@ -5,21 +5,50 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.navigation.NavHostController
 import com.abdurrahmanbulut.composeAppBase.navigator.screen.AppNavigation
 import com.abdurrahmanbulut.composeAppBase.ui.theme.ComposeAppBaseTheme
 import com.abdurrahmanbulut.sherlock.navigation.Navigator
 
+
 val LocalNavigator = staticCompositionLocalOf<Navigator> { error("No Navigator provided") }
+internal val LocalMainActivity = compositionLocalOf<MainActivity> { error("need CoreActivity") }
+internal val LocalNavHostController = compositionLocalOf<NavHostController> { error("need CoreActivity") }
+
+
+@Composable
+@ReadOnlyComposable
+internal fun coreActivity() = LocalMainActivity.current
+
+@Composable
+@ReadOnlyComposable
+internal fun navigator() = LocalNavigator.current
+@Composable
+@ReadOnlyComposable
+internal fun navHostController() = LocalNavHostController.current
 
 class MainActivity : ComponentActivity() {
+    private lateinit var navigator: Navigator
+    lateinit var navHostController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ComposeAppBaseTheme {
-                Surface {
-                    AppNavigation()
+                CompositionLocalProvider(
+                    LocalMainActivity provides this,
+                    LocalNavigator provides navigator,
+                    LocalNavHostController provides navHostController
+                ) {
+                    Surface {
+                        AppNavigation()
+                    }
                 }
             }
         }
